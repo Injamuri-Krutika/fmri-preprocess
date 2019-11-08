82176f08069b030d89e82f6adb6e3dfcdd5579bb
Krutika Injamuri <kruteeka.injamuri@gmail.com>
2019-11-08 15:03:16 +0530

    Script to run 1st level analysis

--- src/run_1stLevel_Analysis.sh
+++ src/run_1stLevel_Analysis.sh
@@ -4,22 +4,25 @@
 # to run just a subset of subjects easier.
 
 for id in `seq -w 1 5` ; do
-    subj="sub-0$id"
-    echo "===> Starting processing of $subj"
-    echo
-    cd $subj/func
-        #Check whether the file subjList.txt exists; if not, create it
-        if [ ! -f designList.txt ]; then
-                ls -d design_run?? > designList.txt
+        subj="sub-0$id"
+        echo "===> Starting processing of $subj"
+        cd ../data/$subj
+        if [ ! -f sessions.txt ]; then
+                ls -d ses-perceptionTraining?? > sessions.txt
         fi
-
-        #Loop over all subjects and format timing files into FSL format
-        for designFile in `cat designList.txt` ; do
-                echo "===> Starting feat for "+designFile
-                feat designFile
-
-    # Go back to the directory containing all of the subjects, and repeat the loop
-    cd ../../
+        for session_dir in `cat sessions.txt`; do
+                cd ./$session_dir/func
+                #Check whether the file subjList.txt exists; if not, create it
+                if [ ! -f designList.txt ]; then
+                        ls -f *.fsf > designList.txt
+                fi
+                Loop over all subjects and format timing files into FSL format
+                for designFile in `cat designList.txt` ; do
+                        echo "===> Starting feat for $designFile"
+                        feat $designFile
+                done
+                cd ../../
+        done
+        # Go back to the directory containing all of the subjects, and repeat the loop
+        cd ../
 done
-
-echo
